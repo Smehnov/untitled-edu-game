cbd76de924dad74c5bc4d5e888b85dc7aca5d6b7
Alex <alexander.smehnov@gmail.com>
2019-10-12 23:47:18 +0300

    add: run-button v1.0

--- level1.js
+++ level1.js
@@ -16,20 +16,29 @@ document.getElementById("run_level_but").onclick = ()=>{
     var output = layer.findOne('.output_brick')
     var output_input_dot = output.findOne('.dot_input')
     console.log(output_input_dot);
+    var span_result = document.getElementById('result')
+    var found_link = false
     for (let i = 0; i < dot_links.length; i++) {
         arr = dot_links[i];
         console.log(arr);
         
         if(arr[1]==output_input_dot){
+            found_link = true
             var html_brick = arr[0].findAncestor('.html_brick')
             if(html_brick.findOne(".input_text").text()=="first_post.html"){
-                console.log(1);
+                span_result.innerHTML = "Читатель смог получить пост Васи, правда этим читателем был сам Вася("
+                // УРОВЕНЬ ПРОЙДЕН
                 
+            }else{
+                span_result.innerHTML = "Файл first_post.html не передан в Server Response <br>"
             }
             
             
         }
         
+    }
+    if(!found_link){
+        span_result.innerHTML = "Ничего не передано в Server Response <br>"
     }
     
      
