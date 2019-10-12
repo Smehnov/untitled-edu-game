8c9a4278b1bed626919d6634a4be1d192e3acc9e
Daniil <boiko.81@list.ru>
2019-10-12 11:31:19 +0300

    add: first templates

--- templates.js
+++ templates.js
@@ -1,9 +1,33 @@
+var stage_template = {
+  container: 'container',
+  width: window.innerWidth,
+  height: window.innerHeight,
+  dragable:true
+}
+
 var block_template = {
     x: 20,
     y: 20,
     width: 100,
     height: 50,
-    fill: 'green',
+    fill: '#00D2FF',
     stroke: 'black',
-    strokeWidth: 4
+    strokeWidth: 4,
+    shadowOffsetX : 5,
+    shadowOffsetY : 3,
+    shadowBlur : 20,
+    opacity : 0.5,
+    cornerRadius: [5, 5, 5, 5]
   }
+
+var arrow_template = {
+  x: 140,
+  y: 180,
+  points: [10, 235, 100, 235],
+  pointerLength: 20,
+  pointerWidth: 20,
+  fill: 'white',
+  stroke: 'black',
+  strokeWidth: 4,
+  id: 'arrow'
+}  
