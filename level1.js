9c9e96f60c1f527ce4bfad8b97539e95d354d942
Alex <alexander.smehnov@gmail.com>
2019-10-12 19:35:33 +0300

    add: level1.js

---
+++ level1.js
@@ -0,0 +1,15 @@
+var width = window.innerWidth;
+var height = window.innerHeight;
+
+var stage = new Konva.Stage(stage_template);
+
+var layer = new Konva.Layer();
+
+
+
+
+
+// add the shape to the layer
+
+
+stage.add(layer);
