facf279a0bba0f8a1839b63f7f7aa67a567aff06
Alex <alexander.smehnov@gmail.com>
2019-10-12 23:32:00 +0300

    add: run-button v0.9

--- level1.js
+++ level1.js
@@ -12,7 +12,26 @@ spawn_output_brick()
 
 
 document.getElementById("run_level_but").onclick = ()=>{
-     var res = false;
+    var res = false;
+    var output = layer.findOne('.output_brick')
+    var output_input_dot = output.findOne('.dot_input')
+    console.log(output_input_dot);
+    for (let i = 0; i < dot_links.length; i++) {
+        arr = dot_links[i];
+        console.log(arr);
+        
+        if(arr[1]==output_input_dot){
+            var html_brick = arr[0].findAncestor('.html_brick')
+            if(html_brick.findOne(".input_text").text()=="first_post.html"){
+                console.log(1);
+                
+            }
+            
+            
+        }
+        
+    }
+    
      
 }
 // add the shape to the layer
